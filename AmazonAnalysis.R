9e2a5cd80748964c00ab0628715fd38dbc16251a
rigvic <144279050+rigvic@users.noreply.github.com>
2023-10-18 13:28:56 -0600

    Add files via upload

--- AmazonAnalysis.R
+++ AmazonAnalysis.R
@@ -1,12 +1,14 @@
 library(tidyverse)
 library(tidymodels)
 library(vroom)
-library(poissonreg)
+# library(poissonreg)
 library(glmnet)
 library(rpart)
 library(ranger)
-library(stacks)
+# library(stacks)
 library(embed)
+install.packages('discrim')
+library(discrim)
 
 amazon_train <- vroom("./train.csv")
 amazon_test <- vroom("./test.csv")
@@ -36,11 +38,12 @@ amazon_workflow <- workflow() %>%
 amazon_predictions <- predict(amazon_workflow,
                               new_data= amazon_test,
                               type = "prob") # "class" or "prob" (see doc)
+amazon_predictions
 
 amazon_predictions <- amazon_predictions %>%
   bind_cols(., amazon_test) %>%
-  select(id, .pred_class) %>%
-  rename(action = .pred_class)
+  select(id, .pred_1) %>%
+  rename(action = .pred_1)
 
 vroom_write(x=amazon_predictions, file="amazon_predictions.csv", delim=",")
 
@@ -89,3 +92,94 @@ pen_predictions <- pen_predictions %>%
 vroom_write(x=pen_predictions, file="pen_predictions.csv", delim=",")
 
 bestTune
+
+# RANDOM FOREST
+my_recipe <- recipe(ACTION ~. , data = amazon_train) %>%
+  step_mutate_at(all_numeric_predictors(), fn = factor) %>%
+  step_other(all_nominal_predictors(), threshold = 0.001) %>%
+  # step_dummy(all_nominal_predictors())
+  step_lencode_mixed(all_nominal_predictors(), outcome=vars(ACTION))
+
+rf_mod <- rand_forest(mtry = tune(),
+                      min_n = tune(),
+                      trees = 500) %>%
+  set_engine("ranger") %>%
+  set_mode("classification")
+  
+
+rf_wf <- workflow() %>%
+  add_recipe(my_recipe) %>%
+  add_model(rf_mod)
+
+tuning_grid <- grid_regular(mtry(range=c(1,ncol(amazon_train)-1)),
+                            min_n(),
+                            levels = 5)
+
+folds <- vfold_cv(amazon_train, v = 10, repeats = 1)
+
+CV_results <- rf_wf %>%
+  tune_grid(resamples = folds,
+            grid = tuning_grid,
+            metrics = metric_set(roc_auc))
+
+bestTune <- CV_results %>%
+  select_best("roc_auc")
+
+final_wf <-
+  rf_wf %>%
+  finalize_workflow(bestTune) %>%
+  fit(data = amazon_train)
+
+rf_predictions <- final_wf %>%
+  predict(amazon_test, type = "prob")
+
+rf_predictions <- rf_predictions %>%
+  bind_cols(., amazon_test) %>%
+  select(id, .pred_1) %>%
+  rename(action = .pred_1)
+
+vroom_write(x=rf_predictions, file="rf_predictions.csv", delim=",")
+
+# NAIVE BAYES
+my_recipe <- recipe(ACTION ~. , data = amazon_train) %>%
+  step_mutate_at(all_numeric_predictors(), fn = factor) %>%
+  step_other(all_nominal_predictors(), threshold = 0.001) %>%
+  # step_dummy(all_nominal_predictors())
+  step_lencode_mixed(all_nominal_predictors(), outcome=vars(ACTION))
+
+nb_mod <- naive_Bayes(Laplace = tune(), smoothness = tune()) %>%
+  set_mode("classification") %>%
+  set_engine("naivebayes")
+
+nb_wf <- workflow() %>%
+  add_recipe(my_recipe) %>%
+  add_model(nb_mod)
+
+tuning_grid <- grid_regular(Laplace(),
+                            smoothness(),
+                            levels = 5)
+
+folds <- vfold_cv(amazon_train, v = 10, repeats = 1)
+
+CV_results <- nb_wf %>%
+  tune_grid(resamples = folds,
+            grid = tuning_grid,
+            metrics = metric_set(roc_auc))
+
+bestTune <- CV_results %>%
+  select_best("roc_auc")
+
+final_wf <-
+  nb_wf %>%
+  finalize_workflow(bestTune) %>%
+  fit(data = amazon_train)
+
+nb_predictions <- final_wf %>%
+  predict(amazon_test, type = "prob")
+
+nb_predictions <- nb_predictions %>%
+  bind_cols(., amazon_test) %>%
+  select(id, .pred_1) %>%
+  rename(action = .pred_1)
+
+vroom_write(x=nb_predictions, file="nb_predictions.csv", delim=",")
