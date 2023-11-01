c408d44ab43f1089ba1d304657131aa3aac9ec6b
rigvic <144279050+rigvic@users.noreply.github.com>
2023-11-01 13:30:48 -0600

    Add files via upload

--- AmazonAnalysis.R
+++ AmazonAnalysis.R
@@ -7,8 +7,10 @@ library(rpart)
 library(ranger)
 # library(stacks)
 library(embed)
-install.packages('discrim')
 library(discrim)
+library(kknn)
+library(themis)
+
 
 amazon_train <- vroom("./train.csv")
 amazon_test <- vroom("./test.csv")
@@ -17,7 +19,7 @@ amazon_train$ACTION <- as.factor(amazon_train$ACTION)
 
 my_recipe <- recipe(ACTION ~. , data = amazon_train) %>%
   step_mutate_at(all_numeric_predictors(), fn = factor) %>%
-  step_other(all_nominal_predictors(), threshold = 0.001) %>%
+  # step_other(all_nominal_predictors(), threshold = 0.001) %>%
   # step_dummy(all_nominal_predictors())
   step_lencode_mixed(all_nominal_predictors(), outcome=vars(ACTION))
 
@@ -96,9 +98,11 @@ bestTune
 # RANDOM FOREST
 my_recipe <- recipe(ACTION ~. , data = amazon_train) %>%
   step_mutate_at(all_numeric_predictors(), fn = factor) %>%
-  step_other(all_nominal_predictors(), threshold = 0.001) %>%
+  # step_other(all_nominal_predictors(), threshold = 0.001) %>%
   # step_dummy(all_nominal_predictors())
-  step_lencode_mixed(all_nominal_predictors(), outcome=vars(ACTION))
+  step_lencode_mixed(all_nominal_predictors(), outcome=vars(ACTION)) %>%
+  step_pca(all_predictors(), threshold = .8) %>%
+  step_smote(all_outcomes(), neighbors = 2)
 
 rf_mod <- rand_forest(mtry = tune(),
                       min_n = tune(),
@@ -183,3 +187,131 @@ nb_predictions <- nb_predictions %>%
   rename(action = .pred_1)
 
 vroom_write(x=nb_predictions, file="nb_predictions.csv", delim=",")
+
+# KNN
+my_recipe <- recipe(ACTION ~. , data = amazon_train) %>%
+  step_mutate_at(all_numeric_predictors(), fn = factor) %>%
+  step_other(all_nominal_predictors(), threshold = 0.001) %>%
+  # step_dummy(all_nominal_predictors())
+  step_lencode_mixed(all_nominal_predictors(), outcome=vars(ACTION)) %>%
+  step_normalize(all_numeric_predictors())
+
+prep <- prep(my_recipe)
+baked_train <- bake(prep, new_data = amazon_train)
+baked_test <- bake(prep, new_data = amazon_test)
+
+knn_mod <- nearest_neighbor(neighbors = tune()) %>%
+  set_mode("classification") %>%
+  set_engine("kknn")
+
+knn_wf <- workflow() %>%
+  add_recipe(my_recipe) %>%
+  add_model(knn_mod)
+
+tuning_grid <- grid_regular(neighbors(),
+                            levels = 5)
+
+folds <- vfold_cv(amazon_train, v = 10, repeats = 1)
+
+CV_results <- knn_wf %>%
+  tune_grid(resamples = folds,
+            grid = tuning_grid,
+            metrics = metric_set(roc_auc))
+
+bestTune <- CV_results %>%
+  select_best("roc_auc")
+
+final_wf <-
+  knn_wf %>%
+  finalize_workflow(bestTune) %>%
+  fit(data = amazon_train)
+
+knn_predictions <- final_wf %>%
+  predict(amazon_test, type = "prob")
+
+knn_predictions <- knn_predictions %>%
+  bind_cols(., amazon_test) %>%
+  select(id, .pred_1) %>%
+  rename(action = .pred_1)
+
+vroom_write(x=knn_predictions, file="knn_predictions.csv", delim=",")
+
+# PCA
+my_recipe <- recipe(ACTION ~. , data = amazon_train) %>%
+  step_mutate_at(all_numeric_predictors(), fn = factor) %>%
+  step_other(all_nominal_predictors(), threshold = 0.001) %>%
+  step_dummy(all_nominal_predictors()) %>%
+  step_lencode_mixed(all_nominal_predictors(), outcome=vars(ACTION)) %>%
+  step_normalize(all_numeric_predictors()) %>%
+  step_pca(all_predictors(), threshold = 0.85)
+  
+prep <- prep(my_recipe)
+baked_train <- bake(prep, new_data = amazon_train)
+baked_test <- bake(prep, new_data = amazon_test)
+
+# SVM
+my_recipe <- recipe(ACTION ~. , data = amazon_train) %>%
+  step_mutate_at(all_numeric_predictors(), fn = factor) %>%
+  step_other(all_nominal_predictors(), threshold = 0.001) %>%
+  step_dummy(all_nominal_predictors()) %>%
+  step_lencode_mixed(all_nominal_predictors(), outcome=vars(ACTION)) %>%
+  step_normalize(all_numeric_predictors())
+
+
+prep <- prep(my_recipe)
+baked_train <- bake(prep, new_data = amazon_train)
+baked_test <- bake(prep, new_data = amazon_test)
+
+svm_poly <- svm_poly(degree = tune(), cost = tune()) %>%
+  set_mode("classification") %>%
+  set_engine("kernlab")
+
+svm_radial <- svm_rbf(rbf_sigma = tune(), cost = tune()) %>%
+  set_mode("classification") %>%
+  set_engine("kernlab")
+
+svm_linear <- svm_linear(cost = tune()) %>%
+  set_mode("classification") %>%
+  set_engine("kernlab")
+
+svm_wf <- workflow() %>%
+  add_recipe(my_recipe) %>%
+  add_model(svm_poly)
+
+tuning_grid <- grid_regular(degree(),
+                            cost(),
+                            levels = 5)
+
+folds <- vfold_cv(amazon_train, v = 10, repeats = 1)
+
+CV_results <- svm_wf %>%
+  tune_grid(resamples = folds,
+            grid = tuning_grid,
+            metrics = metric_set(roc_auc))
+
+ bestTune <- CV_results %>%
+  select_best("roc_auc")
+
+final_wf <-
+  svm_wf %>%
+  finalize_workflow(bestTune) %>%
+  fit(data = amazon_train)
+
+svm_predictions <- final_wf %>%
+  predict(amazon_test, type = "prob")
+
+svm_predictions <- svm_predictions %>%
+  bind_cols(., amazon_test) %>%
+  select(id, .pred_1) %>%
+  rename(action = .pred_1)
+
+vroom_write(x=svm_predictions, file="svm_predictions.csv", delim=",")
+
+# Imbalanced
+my_recipe <- recipe(ACTION ~., data = amazon_train) %>%
+  step_mutate_at(all_numeric_predictors(), fn = factor) %>%
+  step_lencode_mixed(all_nominal_predictors(), outcome = vars(ACTION)) %>%
+  step_normalize(all_numeric_predictors()) %>%
+  step_pca(all_predictors(), threshold = 0.85) %>%
+  step_smote(all_outcomes(), neighbors = 2)
+
